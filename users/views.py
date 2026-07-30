803eb43a6d6728a4931bf9ba2d5c6e2b71ddc001
coderishani <ishanijain237@gmail.com>
2026-07-30 13:43:27 +0530

    fixing errors

--- users/views.py
+++ users/views.py
@@ -12,15 +12,19 @@ def home(request):
 def signup(request):
     if request.method == "POST":
         form = SignUpForm(request.POST)
+
         if form.is_valid():
-            user=form.save()
+            user = form.save()
+            login(request, user)
             return redirect("home")
+
     else:
-        
+        form = SignUpForm()
 
     return render(request, "signup.html", {
         "form": form
     })
+
 def user_login(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
